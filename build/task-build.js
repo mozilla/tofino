6bde2f5c7b915a825a11791f0eb64d8c24d168c7
Victor Porof <vporof@mozilla.com>
2016-04-12 08:34:16 +0200

    Use top level `require` for build-config in task-build.js
    Signed-off-by: Victor Porof <vporof@mozilla.com>

--- build/task-build.js
+++ build/task-build.js
@@ -7,12 +7,10 @@ const webpack = require('webpack');
 
 const webpackProdConfig = require('./webpack.config.prod');
 const webpackDevConfig = require('./webpack.config.dev');
+const { development } = require('../build-config');
 
 module.exports = () => new Promise((resolve, reject) => {
-  const config = require('../build-config').development
-    ? webpackDevConfig
-    : webpackProdConfig;
-
+  const config = development ? webpackDevConfig : webpackProdConfig;
   try {
     const compiler = webpack(config);
     compiler.run(resolve);
