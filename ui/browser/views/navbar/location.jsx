279ff02110f5f52bf3d3a3752952384a313c61e9
Jordan Santell <jordan@jsantell.com>
2016-04-12 17:05:20 -0700

    Display the page's title normally, and transform into a URL bar upon focus, as per UX mockups

--- ui/browser/views/navbar/location.jsx
+++ ui/browser/views/navbar/location.jsx
@@ -10,7 +10,7 @@ CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 */
 
-import React, { PropTypes, Component } from 'react';
+import React, { PropTypes, createClass } from 'react';
 import Btn from './btn.jsx';
 import { fixURL, getCurrentWebView } from '../../browser-util';
 
@@ -20,15 +20,60 @@ import { fixURL, getCurrentWebView } from '../../browser-util';
  * to ipcRenderer on mount, and because the key-down event handler is non
  * trivial.
  */
-class Location extends Component {
-  constructor() {
-    super();
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-  }
+const Location = createClass({
+
+  propTypes: {
+    page: PropTypes.object.isRequired,
+    onLocationChange: PropTypes.func.isRequired,
+    onLocationContextMenu: PropTypes.func.isRequired,
+    onLocationReset: PropTypes.func.isRequired,
+    bookmark: PropTypes.func.isRequired,
+    unbookmark: PropTypes.func.isRequired,
+    ipcRenderer: PropTypes.object.isRequired,
+  },
+
+  getInitialState() {
+    return {
+      showURLBar: false,
+    };
+  },
 
   componentDidMount() {
     this.props.ipcRenderer.on('focus-urlbar', () => this.refs.input.select());
-  }
+  },
+
+  componentDidUpdate() {
+    // If we're showing the URL bar, it should be focused. The scenario
+    // where this isn't true is immediately after displaying the URL bar,
+    // so give it focus
+    if (this.state.showURLBar && document.activeElement !== this.refs.input) {
+      this.refs.input.focus();
+    }
+  },
+
+  /*
+  shouldComponentUpdate(nextProps) {
+    // Currently only update if more changed other than what
+    // the user typed into the URL bar
+    return nextProps.page.userTyped === this.props.page.userTyped;
+  },
+  */
+
+  onTitleClick() {
+    this.setState({ showURLBar: true });
+  },
+
+  onTitleFocus() {
+    this.setState({ showURLBar: true });
+  },
+
+  onURLBarFocus() {
+    this.refs.input.select();
+  },
+
+  onURLBarBlur() {
+    this.setState({ showURLBar: false });
+  },
 
   handleKeyDown(ev) {
     if (ev.keyCode === 13) { // enter
@@ -40,11 +85,12 @@ class Location extends Component {
       this.props.onLocationReset();
       ev.target.select();
     }
-  }
+  },
 
   render() {
     const { page, onLocationChange, onLocationContextMenu, bookmark, unbookmark } = this.props;
-    const value = page.userTyped !== null ? page.userTyped : page.location;
+    const urlValue = page.userTyped !== null ? page.userTyped : page.location;
+    const { showURLBar } = this.state;
 
     const onBookmark = e => {
       const webview = getCurrentWebView(e.target.ownerDocument);
@@ -57,35 +103,56 @@ class Location extends Component {
       }
     };
 
+
     return (
-      <div id="browser-location-bar">
-        <input ref="input"
-          id="urlbar-input"
-          type="text"
-          value={value}
+      <div id="browser-location-bar"
+        style={{
+          flex: 1,
+          display: 'flex',
+          margin: '0 5px',
+        }}>
+        <Btn title="Info"
+          image={''}
+          clickHandler={function() {}}
+          style={{
+            flex: 1,
+            display: 'flex',
+          }} />
+        <span id="browser-location-title-bar"
+          tabIndex={0}
+          onClick={this.onTitleClick}
+          onFocus={this.onTitleFocus}
+          style={{
+            flex: 1,
+            display: showURLBar ? 'none' : 'flex',
+          }}>
+          {page.title}
+        </span>
+        <input id="urlbar-input" type="text" ref="input"
+          onFocus={this.onURLBarFocus}
+          onBlur={this.onURLBarBlur}
+          showURLBar={showURLBar}
+          style={{
+            flex: 1,
+            display: showURLBar ? 'flex' : 'none',
+          }}
+          defaultValue={urlValue}
           onChange={onLocationChange}
-          clickHandler={ev => ev.target.select()}
-          onContextMenu={onLocationContextMenu}
-          onKeyDown={this.handleKeyDown} />
+          onKeyDown={this.handleKeyDown}
+          onContextMenu={onLocationContextMenu} />
 
         <Btn title="Bookmark"
           image={page.isBookmarked
             ? 'glyph-bookmark-filled-16.svg'
             : 'glyph-bookmark-hollow-16.svg'}
-          clickHandler={onBookmark} />
+          clickHandler={onBookmark}
+          style={{
+            flex: 1,
+            display: 'flex',
+          }} />
       </div>
     );
-  }
-}
-
-Location.propTypes = {
-  page: PropTypes.object.isRequired,
-  onLocationChange: PropTypes.func.isRequired,
-  onLocationContextMenu: PropTypes.func.isRequired,
-  onLocationReset: PropTypes.func.isRequired,
-  bookmark: PropTypes.func.isRequired,
-  unbookmark: PropTypes.func.isRequired,
-  ipcRenderer: PropTypes.object.isRequired,
-};
+  },
+});
 
 export default Location;
