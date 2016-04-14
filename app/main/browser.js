0a01f22e269c128bcc1cf47694e12d9209513495
Victor Porof <vporof@mozilla.com>
2016-04-14 15:29:29 +0200

    Use native module syntax in app main.js code, part 3: Properly indent browser.js code
    Signed-off-by: Victor Porof <vporof@mozilla.com>

--- app/main/browser.js
+++ app/main/browser.js
@@ -28,140 +28,140 @@ const root = path.dirname(__dirname);
 const staticDir = path.join(__dirname, '..', '..', 'static');
 
 export default function(browserStartTime) {
-// Keep a global references of the window objects, if you don't, the windows will
-// be closed automatically when the JavaScript object is garbage collected.
-const mainWindows = [];
+  // Keep a global references of the window objects, if you don't, the windows will
+  // be closed automatically when the JavaScript object is garbage collected.
+  const mainWindows = [];
 
-function createWindow(tabInfo) {
-  // Create the browser window.
-  const browser = new BrowserWindow({
-    center: false,
-    width: 1024,
-    height: 720,
-    frame: false,
-    show: false,
-  });
-  mainWindows.push(browser);
+  function createWindow(tabInfo) {
+    // Create the browser window.
+    const browser = new BrowserWindow({
+      center: false,
+      width: 1024,
+      height: 720,
+      frame: false,
+      show: false,
+    });
+    mainWindows.push(browser);
 
-  browser.loadURL('http://localhost:8765/browser.html');
+    browser.loadURL('http://localhost:8765/browser.html');
 
-  if (!isProduction()) {
-    browser.openDevTools({ detach: true });
+    if (!isProduction()) {
+      browser.openDevTools({ detach: true });
+    }
+
+    // Emitted when the window is closed.
+    browser.on('closed', () => {
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      const index = mainWindows.indexOf(browser);
+      if (index > -1) {
+        mainWindows.splice(index, 1);
+      }
+    });
+
+    electronLocalshortcut.register(browser, 'CmdOrCtrl+L', () => {
+      browser.webContents.send('focus-urlbar');
+    });
+
+    electronLocalshortcut.register(browser, 'CmdOrCtrl+R', () => {
+      browser.webContents.send('page-reload');
+    });
+
+    browser.webContents.once('did-finish-load', () => {
+      const browserDidFinishLoadTime = Date.now();
+      instrument.event('browser', 'READY', 'ms', browserDidFinishLoadTime - browserStartTime);
+
+      browser.show();
+
+      if (tabInfo) {
+        browser.webContents.send('tab-attach', tabInfo);
+      } else {
+        browser.webContents.send('new-tab');
+      }
+    });
   }
 
-  // Emitted when the window is closed.
-  browser.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    const index = mainWindows.indexOf(browser);
-    if (index > -1) {
-      mainWindows.splice(index, 1);
-    }
-  });
+  function registerFileProtocol(scheme, mapper) {
+    const protocol = electron.protocol;
 
-  electronLocalshortcut.register(browser, 'CmdOrCtrl+L', () => {
-    browser.webContents.send('focus-urlbar');
-  });
-
-  electronLocalshortcut.register(browser, 'CmdOrCtrl+R', () => {
-    browser.webContents.send('page-reload');
-  });
-
-  browser.webContents.once('did-finish-load', () => {
-    const browserDidFinishLoadTime = Date.now();
-    instrument.event('browser', 'READY', 'ms', browserDidFinishLoadTime - browserStartTime);
-
-    browser.show();
-
-    if (tabInfo) {
-      browser.webContents.send('tab-attach', tabInfo);
-    } else {
-      browser.webContents.send('new-tab');
-    }
-  });
-}
-
-function registerFileProtocol(scheme, mapper) {
-  const protocol = electron.protocol;
-
-  protocol.registerFileProtocol(scheme, (request, callback) => {
-    let url = request.url.substr(scheme.length + 3);
-    let pos = url.indexOf('?');
-    if (pos >= 0) {
-      url = url.substring(0, pos);
-    }
-    pos = url.indexOf('#');
-    if (pos >= 0) {
-      url = url.substring(0, pos);
-    }
-    const file = mapper(url);
-    callback({ path: file });
-  }, error => {
-    if (error) {
-      console.error(`Failed to register ${scheme} protocol`, error);
-    }
-  });
-}
-
-const appStartupTime = Date.now();
-instrument.event('app', 'STARTUP');
-
-// Kick off the internal webserver so we have HMR
-server.serve(staticDir);
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-app.on('ready', () => {
-  const appReadyTime = Date.now();
-  instrument.event('app', 'READY', 'ms', appReadyTime - appStartupTime);
-
-  registerFileProtocol('atom', url => {
-    if (url.indexOf('/') === '-1') {
-      url = `${url}/index.html`;
-    }
-    return path.normalize(path.join(root, 'content', url));
-  });
-  registerFileProtocol('libs', url => path.normalize(path.join(root, 'libs', url)));
-
-  createWindow();
-});
-
-// Unregister all shortcuts.
-app.on('will-quit', () => globalShortcut.unregisterAll());
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
+    protocol.registerFileProtocol(scheme, (request, callback) => {
+      let url = request.url.substr(scheme.length + 3);
+      let pos = url.indexOf('?');
+      if (pos >= 0) {
+        url = url.substring(0, pos);
+      }
+      pos = url.indexOf('#');
+      if (pos >= 0) {
+        url = url.substring(0, pos);
+      }
+      const file = mapper(url);
+      callback({ path: file });
+    }, error => {
+      if (error) {
+        console.error(`Failed to register ${scheme} protocol`, error);
+      }
+    });
   }
 
-  // Set a simple menu since all browser windows are closed.
-  BrowserMenu.default();
-});
+  const appStartupTime = Date.now();
+  instrument.event('app', 'STARTUP');
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindows.length === 0) {
+  // Kick off the internal webserver so we have HMR
+  server.serve(staticDir);
+
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  app.on('ready', () => {
+    const appReadyTime = Date.now();
+    instrument.event('app', 'READY', 'ms', appReadyTime - appStartupTime);
+
+    registerFileProtocol('atom', url => {
+      if (url.indexOf('/') === '-1') {
+        url = `${url}/index.html`;
+      }
+      return path.normalize(path.join(root, 'content', url));
+    });
+    registerFileProtocol('libs', url => path.normalize(path.join(root, 'libs', url)));
+
     createWindow();
-  }
-});
+  });
 
-ipc.on('instrument-event', (event, args) => {
-  // Until we transpile app/, we can't destructure in the argument list or inline here.
-  instrument.event(args.name, args.method, args.label, args.value);
-});
+  // Unregister all shortcuts.
+  app.on('will-quit', () => globalShortcut.unregisterAll());
 
-ipc.on('new-window', createWindow);
+  // Quit when all windows are closed.
+  app.on('window-all-closed', () => {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
 
-ipc.on('window-ready', event => {
-  BrowserWindow.fromWebContents(event.sender).show();
-});
+    // Set a simple menu since all browser windows are closed.
+    BrowserMenu.default();
+  });
 
-ipc.on('update-menu', (event, data) => BrowserMenu.build(data));
+  app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindows.length === 0) {
+      createWindow();
+    }
+  });
 
-ipc.on('tab-detach', (event, tabInfo) => createWindow(tabInfo));
+  ipc.on('instrument-event', (event, args) => {
+    // Until we transpile app/, we can't destructure in the argument list or inline here.
+    instrument.event(args.name, args.method, args.label, args.value);
+  });
+
+  ipc.on('new-window', createWindow);
+
+  ipc.on('window-ready', event => {
+    BrowserWindow.fromWebContents(event.sender).show();
+  });
+
+  ipc.on('update-menu', (event, data) => BrowserMenu.build(data));
+
+  ipc.on('tab-detach', (event, tabInfo) => createWindow(tabInfo));
 }
