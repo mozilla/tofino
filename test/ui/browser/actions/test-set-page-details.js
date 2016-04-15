4f43fb2385965b941aba124ea05fd88e18577c57
Jordan Santell <jordan@jsantell.com>
2016-04-14 17:38:16 -0700

    Stub out the rest of the action creator tests

---
+++ test/ui/browser/actions/test-set-page-details.js
@@ -0,0 +1,14 @@
+// Any copyright is dedicated to the Public Domain.
+// http://creativecommons.org/publicdomain/zero/1.0/
+
+import expect from 'expect'; // eslint-disable-line
+import configureStore from '../../../../ui/browser/store/store';
+import * as actions from '../../../../ui/browser/actions/main-actions'; // eslint-disable-line
+
+describe('Action - SET_PAGE_DETAILS', () => {
+  beforeEach(function() {
+    this.store = configureStore();
+  });
+
+  it('Should set page details');
+});
