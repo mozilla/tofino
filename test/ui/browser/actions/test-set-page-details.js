e1aa72e71a6c33d57910eb1120194803823080ac
Jordan Santell <jordan@jsantell.com>
2016-04-16 12:58:12 -0700

    Add tests for SET_PAGE_DETAILS to cover a non-number scenario and the default current page -1 scenario. r=vporof

--- test/ui/browser/actions/test-set-page-details.js
+++ test/ui/browser/actions/test-set-page-details.js
@@ -16,6 +16,15 @@ describe('Action - SET_PAGE_DETAILS', () => {
     dispatch(actions.createTab('http://moz2.org'));
     dispatch(actions.createTab('http://moz3.org'));
     dispatch(actions.closeTab(0));
+    dispatch(actions.setPageDetails(0, {
+      title: 'moz1',
+    }));
+    dispatch(actions.setPageDetails(1, {
+      title: 'moz2',
+    }));
+    dispatch(actions.setPageDetails(2, {
+      title: 'moz3',
+    }));
     dispatch(actions.setCurrentTab(0));
   });
 
@@ -36,9 +45,9 @@ describe('Action - SET_PAGE_DETAILS', () => {
     dispatch(actions.setPageDetails(10, {
       title: 'Mozzerella Firefox',
     }));
-    expect(getState().pages.get(0).title).toEqual('New Tab');
-    expect(getState().pages.get(1).title).toEqual('New Tab');
-    expect(getState().pages.get(2).title).toEqual('New Tab');
+    expect(getState().pages.get(0).title).toEqual('moz1');
+    expect(getState().pages.get(1).title).toEqual('moz2');
+    expect(getState().pages.get(2).title).toEqual('moz3');
   });
 
   it('Cannot add new properties to a Page', function() {
@@ -55,5 +64,40 @@ describe('Action - SET_PAGE_DETAILS', () => {
         'setPageDetails with an unknown property should throw');
     }
     expect(getState().pages.get(2).myVerySpecialProperty).toEqual(void 0);
+  });
+
+  it('Should modify the currently selected page if index `-1` given', function() {
+    const { dispatch, getState } = this.store;
+
+    expect(getState().currentPageIndex).toEqual(0);
+    dispatch(actions.setPageDetails(-1, {
+      title: 'Mozzerella Firefox',
+    }));
+    expect(getState().pages.get(0).title).toEqual('Mozzerella Firefox');
+    expect(getState().pages.get(1).title).toEqual('moz2');
+    expect(getState().pages.get(2).title).toEqual('moz3');
+
+    dispatch(actions.setCurrentTab(2));
+    dispatch(actions.setPageDetails(-1, {
+      title: 'pasta pasta pasta',
+    }));
+    expect(getState().pages.get(0).title).toEqual('Mozzerella Firefox');
+    expect(getState().pages.get(1).title).toEqual('moz2');
+    expect(getState().pages.get(2).title).toEqual('pasta pasta pasta');
+  });
+
+  it('only allows numbered `pageIndex`', function() {
+    const { dispatch } = this.store;
+
+    ([null, void 0, '-1', '1', {}]).forEach(val => {
+      try {
+        dispatch(actions.setPageDetails(val, { title: 'New Tab!' }));
+        expect(false).toEqual(true,
+          `setPageDetails with a non-number pageIndex should throw: ${val}`);
+      } catch (e) {
+        expect(true).toEqual(true,
+          `setPageDetails with a non-number pageIndex should throw: ${val}`);
+      }
+    });
   });
 });
