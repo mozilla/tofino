0548553ecd40512f50c4180a11eb6cf204f1a90a
Jordan Santell <jsantell@users.noreply.github.com>
2016-04-20 16:31:39 -0700

    Use a wrapper around including electron in renderer process so we can stub out our own version in electron-ipc-mock. r=bgrins" (#213)

--- app/shared/electron.js
+++ app/shared/electron.js
@@ -10,20 +10,25 @@ CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 */
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { ipcRenderer } from '../../shared/electron';
-import App from './views/app';
-import configureStore from './store/store';
+// Stub out ipcRenderer when electron cannot be found (unit tests)
+let electron;
+try {
+  electron = require('electron');
+} catch (e) {
+  const { ipcMain, ipcRenderer } = require('electron-ipc-mock')();
+  electron = {
+    ipcMain,
+    ipcRenderer,
+    remote: {
+      Menu() {},
+      MenuItem() {},
+    },
+    clipboard: {
+      writeText() {},
+      readText() {},
+    },
+  };
+}
 
-const store = configureStore();
-
-const container = document.getElementById('browser-container');
-
-const app = React.createElement(App);
-const chrome = React.createElement(Provider, { store }, app);
-
-const onRender = () => ipcRenderer.send('window-ready');
-
-ReactDOM.render(chrome, container, onRender);
+const { ipcMain, ipcRenderer, remote, clipboard } = electron;
+export { ipcMain, ipcRenderer, remote, clipboard };
