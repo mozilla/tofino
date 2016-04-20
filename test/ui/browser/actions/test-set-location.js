bddf8510ec7b6590dbd021026236855fde30d32b
Victor Porof <victor.porof@gmail.com>
2016-04-20 08:15:11 +0200

    Remove useless babel imports in tests (#186)
    Signed-off-by: Victor Porof <vporof@mozilla.com>

--- test/ui/browser/actions/test-set-location.js
+++ test/ui/browser/actions/test-set-location.js
@@ -1,9 +1,6 @@
 // Any copyright is dedicated to the Public Domain.
 // http://creativecommons.org/publicdomain/zero/1.0/
 
-import 'babel-polyfill';
-import 'babel-register';
-
 import expect from 'expect'; // eslint-disable-line
 import configureStore from '../../../../app/ui/browser/store/store';
 import * as actions from '../../../../app/ui/browser/actions/main-actions'; // eslint-disable-line
