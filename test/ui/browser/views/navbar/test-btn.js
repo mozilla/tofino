72eed8ab33390548e7c576fc5a805293c4b82077
Victor Porof <vporof@mozilla.com>
2016-04-20 07:09:07 +0200

    Fix typos and remove `noop` from test-btn.js as it is not needed
    Signed-off-by: Victor Porof <vporof@mozilla.com>

--- test/ui/browser/views/navbar/test-btn.js
+++ test/ui/browser/views/navbar/test-btn.js
@@ -6,40 +6,38 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import Btn from '../../../../../app/ui/browser/views/navbar/btn';
-const noop = function() {};
 
 describe('Btn', () => {
   it('sets `id` if given', () => {
     const wrapper = shallow(
       <Btn id={'my-btn'}
-        title={'my title'}
-        clickHandler={noop} />);
+        title={'my title'} />);
     expect(wrapper.prop('id')).toEqual('my-btn');
   });
   it('has `disabled` prop if given', () => {
     const wrapper = shallow(
       <Btn disabled
-        title={'my title'}
-        clickHandler={noop} />);
+        title={'my title'} />);
     expect(wrapper.prop('disabled')).toEqual(true);
   });
   it('does not have `disabled` if not given', () => {
     const wrapper = shallow(
-      <Btn title={'my title'}
-        clickHandler={noop} />);
+      <Btn title={'my title'} />
+    );
     expect(wrapper.prop('disabled')).toEqual(void 0);
   });
   it('sets `title`', () => {
     const wrapper = shallow(
-      <Btn title={'my title'}
-        clickHandler={noop} />);
+      <Btn title={'my title'} />
+    );
     expect(wrapper.prop('title')).toEqual('my title');
   });
   it('fires the click handler on click', () => {
     const spy = expect.createSpy();
     const wrapper = shallow(
       <Btn title={'my title'}
-        clickHandler={spy} />);
+        clickHandler={spy} />
+    );
     expect(spy).toNotHaveBeenCalled();
     wrapper.simulate('click');
     expect(spy).toHaveBeenCalled();
@@ -49,17 +47,17 @@ describe('Btn', () => {
     const wrapper = shallow(
       <Btn disabled
         title={'my title'}
-        clickHandler={spy} />);
+        clickHandler={spy} />
+    );
     expect(spy).toNotHaveBeenCalled();
     wrapper.simulate('click');
     expect(spy).toNotHaveBeenCalled();
   });
-
-  it('sets `id` if given', () => {
+  it('sets `image` if given', () => {
     const wrapper = shallow(
       <Btn image={'some-file.svg'}
-        title={'my title'}
-        clickHandler={noop} />);
+        title={'my title'} />
+    );
     expect(JSON.stringify(wrapper.prop('style'))).toContain('some-file.svg');
   });
 });
