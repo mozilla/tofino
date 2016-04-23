88e89306e8abae0cf1241c43d90f758f4f4b92ba
Lin Clark <lclark@mozilla.com>
2016-04-22 20:51:31 -0400

    Make state dependencies in App more explicit

--- app/ui/browser/views/app.jsx
+++ app/ui/browser/views/app.jsx
@@ -22,16 +22,15 @@ const APP_STYLE = Style.registerStyle({
   height: '100%',
 });
 
-const App = function({ state, dispatch }) {
+const App = function({ profile, currentPageIndex, currentPage, pageAreaVisible, dispatch }) {
   return (
     <div className={APP_STYLE}>
       <BrowserWindow ipcRenderer={ipcRenderer}
         dispatch={dispatch}
-        profile={state.browserWindow.profile}
-        pages={state.browserWindow.pages}
-        currentPageIndex={state.browserWindow.currentPageIndex}
-        currentPage={state.browserWindow.pages.get(state.browserWindow.currentPageIndex)}
-        pageAreaVisible={state.browserWindow.pageAreaVisible} />
+        profile={profile}
+        currentPageIndex={currentPageIndex}
+        currentPage={currentPage}
+        pageAreaVisible={pageAreaVisible} />
       <Style.Element />
     </div>
   );
@@ -40,12 +39,21 @@ const App = function({ state, dispatch }) {
 App.displayName = 'App';
 
 App.propTypes = {
-  state: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+  currentPageIndex: PropTypes.number.isRequired,
+  currentPage: PropTypes.object.isRequired,
+  pageAreaVisible: PropTypes.bool.isRequired,
   dispatch: PropTypes.func.isRequired,
 };
 
 function mapStateToProps(state) {
-  return { state };
+  const browserWindow = state.browserWindow;
+  return {
+    profile: browserWindow.profile,
+    currentPageIndex: browserWindow.currentPageIndex,
+    currentPage: browserWindow.pages.get(browserWindow.currentPageIndex),
+    pageAreaVisible: browserWindow.pageAreaVisible,
+  };
 }
 
 export default connect(mapStateToProps)(Style.component(App));
