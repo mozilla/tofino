9d6a40623974507b9516570ee12b5bc211aadd01
Nick Alexander <ncalexan@users.noreply.github.com>
2016-04-27 18:25:07 -0700

    Make "New Window" a profile command. r=bgrins (#281)
    It's worth noting the setImmediate() in browser-menu.js, which is
    surprising.  I think I'm seeing an interaction with showing the browser
    window and the child rendering event loops.
    
    There's still clean-up to be done here: I expect to bring
    `commandHandler` and `makeBrowserWindow` into a Browser class or
    similar.
    
    And I still don't know how to test these actions.

--- pkg/github-electron-interfaces/electron.js
+++ pkg/github-electron-interfaces/electron.js
@@ -10,9 +10,123 @@
  specific language governing permissions and limitations under the License.
  */
 
+/**
+ * Cribbed from
+ * https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/github-electron/github-electron.d.ts.
+ */
 declare module 'electron' {
+  declare var app: any;
+  declare var globalShortcut: any;
+  declare var ipcMain: any;
+
+	declare interface LoadURLOptions {
+		/**
+		 * HTTP Referrer URL.
+		 */
+		httpReferrer?: string;
+		/**
+		 * User agent originating the request.
+		 */
+		userAgent?: string;
+		/**
+		 * Extra headers separated by "\n"
+		 */
+		extraHeaders?: string;
+	}
+
+  declare interface WebContents {
+		send(channel: string, ...args: any[]): void;
+		once(event: string, listener: Function): WebContents;
+  }
+
   declare class BrowserWindow {
     sessionId: number; // This is Tofino specific.
     didFinishLoadPromise: Promise<void>; // This is Tofino specific.
+
+    webContents: WebContents;
+
+	  loadURL(url: string, options?: LoadURLOptions): void;
+
+    static fromId(id: number): ?BrowserWindow;
+    static fromWebContents(webContents: WebContents): ?BrowserWindow;
+
+		/**
+		 * Get the unique ID of this window.
+		 */
+		id: number;
+		/**
+		 * Force closing the window, the unload and beforeunload event won't be emitted
+		 * for the web page, and close event would also not be emitted for this window,
+		 * but it would guarantee the closed event to be emitted.
+		 * You should only use this method when the renderer process (web page) has crashed.
+		 */
+		destroy(): void;
+		/**
+		 * Try to close the window, this has the same effect with user manually clicking
+		 * the close button of the window. The web page may cancel the close though,
+		 * see the close event.
+		 */
+		close(): void;
+		/**
+		 * Focus on the window.
+		 */
+		focus(): void;
+		/**
+		 * Remove focus on the window.
+		 */
+		blur(): void;
+		/**
+		 * @returns Whether the window is focused.
+		 */
+		isFocused(): boolean;
+		/**
+		 * Shows and gives focus to the window.
+		 */
+		show(): void;
+		/**
+		 * Shows the window but doesn't focus on it.
+		 */
+		showInactive(): void;
+		/**
+		 * Hides the window.
+		 */
+		hide(): void;
+		/**
+		 * @returns Whether the window is visible to the user.
+		 */
+		isVisible(): boolean;
+		/**
+		 * Maximizes the window.
+		 */
+		maximize(): void;
+		/**
+		 * Unmaximizes the window.
+		 */
+		unmaximize(): void;
+		/**
+		 * @returns Whether the window is maximized.
+		 */
+		isMaximized(): boolean;
+		/**
+		 * Minimizes the window. On some platforms the minimized window will be
+		 * shown in the Dock.
+		 */
+		minimize(): void;
+		/**
+		 * Restores the window from minimized state to its previous state.
+		 */
+		restore(): void;
+		/**
+		 * @returns Whether the window is minimized.
+		 */
+		isMinimized(): boolean;
+		/**
+		 * Sets whether the window should be in fullscreen mode.
+		 */
+		setFullScreen(flag: boolean): void;
+		/**
+		 * @returns Whether the window is in fullscreen mode.
+		 */
+		isFullScreen(): boolean;
   }
 }
