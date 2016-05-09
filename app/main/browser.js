41e4d494d9b71185319592dca430e36c124cbc13
Dave Townsend <dtownsend@oxymoronical.com>
2016-05-09 09:23:27 -0700

    Don't attempt to build the menu when the app is quitting.
    Signed-off-by: Dave Townsend <dtownsend@oxymoronical.com>

--- app/main/browser.js
+++ app/main/browser.js
@@ -227,6 +227,7 @@ app.on('window-all-closed', () => {
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit();
+    return;
   }
 
   // Set a simple menu since all browser windows are closed.
