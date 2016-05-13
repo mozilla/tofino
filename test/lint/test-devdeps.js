381429e15cf8eac760217a4eeec495ff76155a1f
Victor Porof <vporof@mozilla.com>
2016-05-13 14:27:44 +0200

    Add a test that makes sure we don't import from devDependencies in app/ code
    Signed-off-by: Victor Porof <vporof@mozilla.com>
    
    tmp
    
    Signed-off-by: Victor Porof <vporof@mozilla.com>

---
+++ test/lint/test-devdeps.js
@@ -0,0 +1,40 @@
+// Any copyright is dedicated to the Public Domain.
+// http://creativecommons.org/publicdomain/zero/1.0/
+
+import expect from 'expect';
+import fs from 'fs-promise';
+import without from 'lodash/without';
+import {
+  REQUIRES_REGEX,
+  IMPORTS_REGEX,
+  autoFailingAsyncTest,
+  globMany,
+  regexFiles,
+} from './shared.js';
+
+const all = '/**/';
+const valid = '*.@(js|jsx)';
+
+const paths = [
+  `app${all}${valid}`,
+];
+
+const ignored = [
+  'electron-ipc-mock',
+  'react-addons-perf',
+];
+
+describe('dev dependecies', () => {
+  it('should be imported in app/', autoFailingAsyncTest(async function() {
+    const manifest = await fs.readJson('package.json');
+    const modules = Object.keys(manifest.devDependencies);
+
+    const sources = await globMany(paths);
+    const matches = await regexFiles(sources, REQUIRES_REGEX, IMPORTS_REGEX);
+    const expected = Array.from(new Set(without(matches, ...ignored)));
+
+    for (const module of modules) {
+      expect(expected).toExclude(module);
+    }
+  }));
+});
