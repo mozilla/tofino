50a7275f71cf885a43be5a7e2a71c12684e91fed
Victor Porof <vporof@mozilla.com>
2016-05-17 13:27:33 -0700

    Create shared prop types to use in all components
    Signed-off-by: Victor Porof <vporof@mozilla.com>

---
+++ app/ui/browser/model/shared-prop-types.js
@@ -0,0 +1,18 @@
+/*
+Copyright 2016 Mozilla
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+*/
+
+import { PropTypes } from 'react';
+import ImmutablePropTypes from 'react-immutable-proptypes';
+import * as Model from './index';
+
+export const Page = PropTypes.instanceOf(Model.Page);
+export const Pages = ImmutablePropTypes.listOf(Page.isRequired);
