9513107496f41198749294db1fd2503b646e4838
Victor Porof <vporof@mozilla.com>
2016-05-17 03:38:30 -0700

    Add more tests for the Button component
    Signed-off-by: Victor Porof <vporof@mozilla.com>

--- test/ui/browser/widgets/test-btn.js
+++ test/ui/browser/widgets/test-btn.js
@@ -5,33 +5,40 @@ import expect from 'expect';
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import Btn from '../../../../app/ui/browser/widgets/btn';
+import Btn, * as Const from '../../../../app/ui/browser/widgets/btn';
 
 describe('Btn', () => {
   it('sets `id` if given', () => {
     const wrapper = shallow(
-      <Btn id={'my-btn'}
+      <Btn id="my-btn"
         clickHandler={() => {}}
-        title={'my title'} />);
+        title="my title" />);
     expect(wrapper.prop('id')).toEqual('my-btn');
+  });
+  it('sets `className` if given', () => {
+    const wrapper = shallow(
+      <Btn className="my-btn-cls"
+        clickHandler={() => {}}
+        title="my title" />);
+    expect(wrapper.prop('className')).toMatch(/my-btn-cls/);
   });
   it('has `disabled` prop if given', () => {
     const wrapper = shallow(
       <Btn disabled
         clickHandler={() => {}}
-        title={'my title'} />);
+        title="my title" />);
     expect(wrapper.prop('disabled')).toEqual(true);
   });
   it('does not have `disabled` if not given', () => {
     const wrapper = shallow(
-      <Btn title={'my title'}
+      <Btn title="my title"
         clickHandler={() => {}} />
     );
     expect(wrapper.prop('disabled')).toEqual(void 0);
   });
   it('sets `title`', () => {
     const wrapper = shallow(
-      <Btn title={'my title'}
+      <Btn title="my title"
         clickHandler={() => {}} />
     );
     expect(wrapper.prop('title')).toEqual('my title');
@@ -39,7 +46,7 @@ describe('Btn', () => {
   it('fires the click handler on click', () => {
     const spy = expect.createSpy();
     const wrapper = shallow(
-      <Btn title={'my title'}
+      <Btn title="my title"
         clickHandler={spy} />
     );
     expect(spy).toNotHaveBeenCalled();
@@ -50,7 +57,7 @@ describe('Btn', () => {
     const spy = expect.createSpy();
     const wrapper = shallow(
       <Btn disabled
-        title={'my title'}
+        title="my title"
         clickHandler={spy} />
     );
     expect(spy).toNotHaveBeenCalled();
@@ -59,10 +66,44 @@ describe('Btn', () => {
   });
   it('sets `image` if given', () => {
     const wrapper = shallow(
-      <Btn image={'some-file.svg'}
+      <Btn image="some-file.svg"
         clickHandler={() => {}}
-        title={'my title'} />
+        title="my title" />
     );
-    expect(JSON.stringify(wrapper.prop('style'))).toContain('some-file.svg');
+    expect(wrapper.prop('style')).toContain({
+      backgroundImage: 'url(assets/some-file.svg)',
+      backgroundRepeat: Const.BKG_REPEAT_DEFAULT,
+      backgroundPosition: Const.BKG_POSIITON_DEFAULT,
+      backgroundSize: Const.BKG_SIZE_DEFAULT,
+    });
+  });
+  it('sets a default `image` if given a falsy prop', () => {
+    const wrapper = shallow(
+      <Btn image=""
+        clickHandler={() => {}}
+        title="my title" />
+    );
+    expect(wrapper.prop('style')).toContain({
+      backgroundImage: `url(${Const.BKG_IMAGE_DEFAULT})`,
+      backgroundRepeat: Const.BKG_REPEAT_DEFAULT,
+      backgroundPosition: Const.BKG_POSIITON_DEFAULT,
+      backgroundSize: Const.BKG_SIZE_DEFAULT,
+    });
+  });
+  it('sets `imgWidth`, `imgHeight` and `imgPosition` if given', () => {
+    const wrapper = shallow(
+      <Btn image=""
+        imgWidth="100px"
+        imgHeight="200px"
+        imgPosition="center"
+        clickHandler={() => {}}
+        title="my title" />
+    );
+    expect(wrapper.prop('style')).toContain({
+      backgroundImage: `url(${Const.BKG_IMAGE_DEFAULT})`,
+      backgroundRepeat: Const.BKG_REPEAT_DEFAULT,
+      backgroundPosition: 'center',
+      backgroundSize: '100px 200px',
+    });
   });
 });
