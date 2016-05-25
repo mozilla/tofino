7d3b43617a52fb651d72d70bb11d7dc04435cd47
Victor Porof <vporof@mozilla.com>
2016-05-25 16:57:33 -0700

    Stop registering a tofino:// file protocol and serve the content pages from our server via http
    Signed-off-by: Victor Porof <vporof@mozilla.com>

--- app/main/browser.js
+++ app/main/browser.js
@@ -33,7 +33,6 @@ import electron from 'electron';
 import * as hotkeys from './hotkeys';
 import * as menu from './menu/index';
 import * as instrument from '../services/instrument';
-import registerAboutPages from './about-pages';
 import * as spawn from './spawn';
 import * as BW from './browser-window';
 import UserAgentClient from '../shared/user-agent-client';
@@ -58,9 +57,6 @@ app.on('ready', async function() {
 
   // Force the menu to be built at least once on startup
   menu.buildAppMenu(menuData);
-
-  // Register `about:*` protocols after app's 'ready' event
-  registerAboutPages();
 
   await BW.createBrowserWindow(userAgentClient, () => {
     instrument.event('browser', 'READY', 'ms', Date.now() - browserStartTime);
