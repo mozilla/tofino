766bf00ba584e73d3626a462a53c857294d8b7a7
Victor Porof <vporof@mozilla.com>
2016-05-26 14:12:26 -0700

    Properly inject content in pages having the about server origin to allow them to query profile data
    Signed-off-by: Victor Porof <vporof@mozilla.com>

--- app/services/user-agent-service/server.js
+++ app/services/user-agent-service/server.js
@@ -16,7 +16,7 @@ import * as profileDiffs from '../../shared/profile-diffs';
 import * as endpoints from '../../shared/constants/endpoints';
 
 const allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'tofino://');
+  res.header('Access-Control-Allow-Origin', endpoints.CONTENT_SERVER_ORIGIN);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
 
