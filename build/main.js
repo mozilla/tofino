a5a64e3a581d7ccbff6c500e4a25e4add5e295bc
Victor Porof <vporof@mozilla.com>
2016-06-04 22:54:02 +0200

    Add more logging to webpack tasks
    Signed-off-by: Victor Porof <vporof@mozilla.com>

--- build/main.js
+++ build/main.js
@@ -15,6 +15,7 @@
 
 require('babel-polyfill');
 require('babel-register')();
+
 const semver = require('semver');
 const checkDependencies = require('check-dependencies');
 const VALID_NODE_VERSION_RANGE = require('../package.json').engines.node;
@@ -27,8 +28,18 @@ const handleDepsCheckFailed = result => {
 
 const handleTaskFailed = err => {
   console.error('Build failed.');
-  console.error(err);
-  process.exit(1);
+
+  // Need log webpack output here because console.log isn't an atomic operation.
+  // Doing this somewhere else before other logging operations will result in
+  // garbled text in stdout. Also, exitting immediately would stop the logging
+  // midway, resulting in incomplete output.
+  if ('webpackStatusOutput' in err) {
+    console.error(err.webpackStatusOutput);
+  } else {
+    console.error(err);
+  }
+
+  process.stdout.once('drain', () => process.exit(1));
 };
 
 const checkNodeVersion = version => {
