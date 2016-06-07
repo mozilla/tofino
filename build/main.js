f77958f4c4ff0721e269663dc5467ba383e62be2
Victor Porof <vporof@mozilla.com>
2016-06-07 18:43:28 +0200

    Log webpack incremental build output
    Signed-off-by: Victor Porof <vporof@mozilla.com>

--- build/main.js
+++ build/main.js
@@ -16,6 +16,7 @@
 require('babel-polyfill');
 require('babel-register')();
 
+const thenify = require('thenify');
 const semver = require('semver');
 const checkDependencies = require('check-dependencies');
 const VALID_NODE_VERSION_RANGE = require('../package.json').engines.node;
@@ -28,18 +29,14 @@ const handleDepsCheckFailed = result => {
 
 const handleTaskFailed = err => {
   console.error('Build failed.');
+  console.error(err);
 
-  // Need log webpack output here because console.log isn't an atomic operation.
-  // Doing this somewhere else before other logging operations will result in
-  // garbled text in stdout. Also, exitting immediately would stop the logging
-  // midway, resulting in incomplete output.
-  if ('webpackStatusOutput' in err) {
-    console.error(err.webpackStatusOutput);
-  } else {
-    console.error(err);
-  }
-
-  process.stdout.once('drain', () => process.exit(1));
+  // Exitting immediately would stop the logging midway,
+  // resulting in incomplete output.
+  Promise.all([
+    thenify(process.stdout.once)('drain'),
+    thenify(process.stderr.once)('drain'),
+  ]).then(() => process.exit(1));
 };
 
 const checkNodeVersion = version => {
--- build/utils.js
+++ build/utils.js
@@ -107,13 +107,11 @@ export async function spawn(command, args, options = {}) {
 }
 
 export function webpackBuild(config) {
+  let incremental = false;
+
   return new Promise((resolve, reject) => {
     const compiler = webpack(config);
     const watcher = compiler.watch({}, (err, stats) => {
-      // Per webpack's documentation, this handler can be called multiple times,
-      // e.g. when a build has been completed, or an error or warning has occurred.
-      // It even can occur that handler is called for the same bundle multiple times.
-      // So just keep that in mind.
       if (err) {
         // Failed with a fatal error.
         reject(err);
@@ -121,30 +119,43 @@ export function webpackBuild(config) {
       }
       if (stats.hasErrors()) {
         // Failed with a build error.
-        reject({
-          webpackStatusOutput: `\n${stats.toString({
-            colors: true,
-            hash: true,
-            version: true,
-            timings: true,
-            assets: true,
-            chunks: true,
-            chunkModules: false,
-            modules: true,
-            children: true,
-            cached: true,
-            reasons: true,
-            source: true,
-            errorDetails: true,
-            chunkOrigins: true,
-          })}\n`,
+        const output = stats.toString({
+          colors: true,
+          hash: true,
+          version: true,
+          timings: true,
+          assets: true,
+          chunks: true,
+          chunkModules: false,
+          modules: true,
+          children: true,
+          cached: true,
+          reasons: true,
+          source: true,
+          errorDetails: true,
+          chunkOrigins: true,
         });
+        console.error(`\n${output}\n`);
+        process.stderr.once('drain', () => reject('Compilation unsuccessful.'));
         return;
       }
+
       /* eslint-disable no-shadow */
       resolve({
         close: () => new Promise(resolve => watcher.close(resolve)),
       });
+
+      // Per webpack's documentation, this handler can be called multiple times,
+      // e.g. when a build has been completed, or an error or warning has occurred.
+      // It even can occur that handler is called for the same bundle multiple times.
+      // So just keep that in mind.
+      if (!incremental) {
+        incremental = true;
+        return;
+      }
+
+      const { time } = stats.toJson();
+      console.log(`Incremental build succeeded in ${time} ms.`);
     });
   });
 }
