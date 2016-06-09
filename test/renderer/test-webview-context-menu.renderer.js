4a36c7d4108ae3036943d8eb75de8ed81fba9d8f
Jordan Santell <jsantell@mozilla.com>
2016-06-09 13:43:58 -0700

    Add tests and stubs for context menu functionality. r=vporof

---
+++ test/renderer/test-webview-context-menu.renderer.js
@@ -0,0 +1,16 @@
+// Any copyright is dedicated to the Public Domain.
+// http://creativecommons.org/publicdomain/zero/1.0/
+
+describe('renderer - webview context menu', function() {
+  it('Open Link in New Tab');
+  it('Copy Link Address');
+  it('Save Image As');
+  it('Copy Image URL');
+  it('Open Image in New Tab');
+  it('Copy');
+  it('Select All');
+  it('Inspect Element');
+  it('Back');
+  it('Forward');
+  it('Reload');
+});
