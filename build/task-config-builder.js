630a96ea94bc916092736d50d1b68c5057585753
Victor Porof <vporof@mozilla.com>
2016-06-14 13:22:24 +0100

    Move the base config into a shared file
    Signed-off-by: Victor Porof <vporof@mozilla.com>

--- build/task-config-builder.js
+++ build/task-config-builder.js
@@ -6,40 +6,8 @@
  * build configurations.
  */
 
-import os from 'os';
-
 import * as BuildUtils from './utils';
-
-export const BASE_CONFIG = {
-  // System information
-  platform: os.platform(),
-  arch: 'x64',
-
-  // Electron information
-  electron: BuildUtils.getElectronVersion(),
-
-  // Version number, with extra build numbers if in a CI environment
-  version: BuildUtils.getAppVersion(),
-
-  // If this build occurred on Travis CI
-  travis: BuildUtils.IS_TRAVIS,
-
-  // If this build occurred on Appveyor
-  appveyor: BuildUtils.IS_APPVEYOR,
-
-  // The `development` option indicates whether or not the build is
-  // using hot reloading and unminified content and other things like that.
-  development: false,
-
-  // Whether or not the build is packaged in an electron distributable
-  packaged: false,
-
-  // Whether or not the build is being tested
-  test: false,
-
-  // The Google Analytics tracking identifier to use when instrumenting.
-  googleAnalyticsTrackingID: 'UA-76122102-1',
-};
+import BASE_CONFIG from './base-config';
 
 export default function(options) {
   let currentConfig = {};
--- test/unit/build/test-tasks.js
+++ test/unit/build/test-tasks.js
@@ -5,7 +5,8 @@ import expect from 'expect';
 import fs from 'fs-promise';
 
 import { autoFailingAsyncTest } from '../../utils/async.js';
-import config, { BASE_CONFIG } from '../../../build/task-config-builder.js';
+import BASE_CONFIG from '../../../build/base-config.js';
+import config from '../../../build/task-config-builder.js';
 import clean from '../../../build/task-clean-package.js';
 import * as utils from '../../../build/utils.js';
 import * as BuildConst from '../../../build/const.js';
