c03629bb2d76960ca625dc2aac774e68aa01017d
Victor Porof <vporof@mozilla.com>
2016-06-14 16:40:03 +0100

    Fix error when trying to run `npm run serve`, regressed by #654
    Signed-off-by: Victor Porof <vporof@mozilla.com>

--- build/tasks.js
+++ build/tasks.js
@@ -63,7 +63,6 @@ export default {
   async serve() {
     await Lazy.config();
     const watchers = [
-      await Lazy.buildShared(),
       await Lazy.buildServices(),
     ];
     await unwatch(watchers);

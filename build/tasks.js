mozilla/tofino

commit 29738c58fde694ecaadf5053c7eafc1bf9abec60

Add bunyan as a logging framework and use it in all non-content processes. r=victor
Bunyan was mainly chosen in preference to winston because winston doesn't work
in a webpacked environment.

When a moduile first needs the logger it will be configured for the process.
Processes that run in a web context log to the regular console, other processes
log to stdout. The UA and content services also log to a file which helps
debugging issues on windows.

Webpack is used to alias 'logging' to the correct logging module for the
process.

build/tasks.js
@@ -9,6 +9,8 @@
  */
  /* eslint global-require: 0 */
 
+import { logger } from './logging';
+
 const Lazy = {
   buildDeps: () => require('./task-build-deps').default(),
   config: options => require('./task-config-builder').default(options),
@@ -24,7 +26,7 @@ const Lazy = {
 };
 
 const unwatch = watchers => {
-  console.log('Stopped watching the filesystem for changes.');
+  logger.info('Stopped watching the filesystem for changes.');
   return Promise.all(watchers.map(w => w.close()));
 };
 
@@ -51,7 +53,7 @@ export default {
       return [];
     }
 
-    console.log('Now watching the filesystem for changes...');
+    logger.info('Now watching the filesystem for changes...');
     return watchers;
   },
 

.eslintrc
@@ -48,6 +48,7 @@
     "arrow-body-style": ["off"],
     "func-names": ["off"],
     "space-before-function-paren": ["error", "never"],
+    "no-console": ["error"],
     "no-confusing-arrow": ["off"],
     "no-param-reassign": ["off"],
     "no-return-assign": ["off"],
@@ -70,7 +71,7 @@
     "react/jsx-handler-names": ["error"],
     "react/prefer-es6-class": ["off"],
     "import/no-unresolved": ["error", {
-      "ignore": ["^electron$", "\\./build-config$"],
+      "ignore": ["^electron$", "\\./build-config$", "^logging$"],
       "commonjs": true
     }],
     "import/no-commonjs": ["error"],

.gitignore
@@ -12,3 +12,4 @@ build-config.json
 .electron
 .cache
 .vscode
+*.log

app/.eslintrc
@@ -1,5 +1,0 @@
-{
-   "rules": {
-     "no-console": ["off"]
-   }
-}

app/main/browser.js
@@ -16,15 +16,16 @@ specific language governing permissions and limitations under the License.
 const browserStartTime = Date.now();
 
 import 'source-map-support/register';
+import { logger } from '../shared/logging';
 
 process.on('uncaughtException', (err) => {
-  console.error(err.stack);
+  logger.error(err.stack);
   process.exit(1);
 });
 
 process.on('unhandledRejection', (reason, p) => {
-  console.error(`Unhandled Rejection at: Promise ${JSON.stringify(p)}`);
-  console.error(reason.stack);
+  logger.error(`Unhandled Rejection at: Promise ${JSON.stringify(p)}`);
+  logger.error(reason.stack);
   process.exit(2);
 });
 

app/main/spawn.js
@@ -12,6 +12,7 @@ specific language governing permissions and limitations under the License.
 
 import { spawn } from 'child_process';
 import path from 'path';
+import { logger } from '../shared/logging';
 import * as endpoints from '../shared/constants/endpoints';
 import BUILD_CONFIG from '../../build-config';
 
@@ -28,12 +29,12 @@ const CONTENT_SERVICE_PATH = path.join(SERVICES_PATH, 'content-service', 'index.
 
 const children = new Set();
 function spawnProcess(name, command, args, options) {
-  console.log(`Executing ${command} ${args.join(' ')}`);
+  logger.debug(`Executing ${command} ${args.join(' ')}`);
   const child = spawn(command, args, options);
   children.add(child);
 
   child.on('error', (error) => {
-    console.error(`${name} threw error ${error}`);
+    logger.error(`${name} threw error ${error}`);
   });
 
   child.on('exit', (code) => {
@@ -46,7 +47,7 @@ function spawnProcess(name, command, args, options) {
 
     // Generally the services shouldn't exit while this process is still running
     // so this is an error regardless of the exit code.
-    console.error(`${name} exited with exit code ${code}`);
+    logger.error(`${name} exited with exit code ${code}`);
   });
 }
 

app/services/common.js
@@ -17,6 +17,7 @@ import expressWs from 'express-ws';
 import expressValidator from 'express-validator';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import { logger } from '../shared/logging';
 
 /**
  * Catch errors from the given async `method` and forward them to `next` for handling.
@@ -33,7 +34,7 @@ export function autoCaughtRouteError({ validator, method }) {
 
       const errors = req.validationErrors();
       if (errors) {
-        console.warn(errors);
+        logger.warn(errors);
         res.status(401).json(errors);
         return;
       }
@@ -82,7 +83,7 @@ export function makeServer(version, port) {
       // Must follow route definitions! Need to have four arguments here
       // so express knows that we want to handle errors.
       app.use((error, req, res, _next) => {
-        console.error(error.stack);
+        logger.error(error.stack);
         res.status(500).json({ error, stack: error.stack });
       });
 

app/services/user-agent-service/datomstore.js
@@ -23,6 +23,7 @@ import {
 import Immutable from 'immutable';
 import microtime from 'microtime-fast';
 import { Bookmark } from '../../shared/model';
+import { logger } from '../../shared/logging';
 
 import { SessionEndReason, SessionStartReason, SnippetSize, StarOp, VisitType } from './storage';
 
@@ -188,7 +189,7 @@ export class ProfileDatomStorage {
     [:find ?title :in $ ?url :where [?x "page/url" ?url] [?x "page/title" ?title]]
     `);
     let results = datascript.core.q(query, datascript.js.db(this.conn), 'http://foo.com/');
-    console.log(results);
+    logger.info(results);
 
     datascript.core.transact_BANG_(this.conn, vector(
       vector(DB_ADD, -1, 'page/url', 'http://foo.com/'),
@@ -196,7 +197,7 @@ export class ProfileDatomStorage {
     ));
 
     results = datascript.core.q(query, datascript.js.db(this.conn), 'http://foo.com/');
-    console.log(results);
+    logger.info(results);
     return results;
   }
 

commit c03629bb2d76960ca625dc2aac774e68aa01017d

Fix error when trying to run `npm run serve`, regressed by #654
Signed-off-by: Victor Porof <vporof@mozilla.com>

build/tasks.js
@@ -63,7 +63,6 @@ export default {
   async serve() {
     await Lazy.config();
     const watchers = [
-      await Lazy.buildShared(),
       await Lazy.buildServices(),
     ];
     await unwatch(watchers);