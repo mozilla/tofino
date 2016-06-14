820ccb18b45d04357cac36c11f6096eb7e28d654
Victor Porof <vporof@mozilla.com>
2016-06-14 12:41:18 +0100

    Stop building the renderer process (browser) when nothing has changed
    Signed-off-by: Victor Porof <vporof@mozilla.com>

--- build/task-build-browser.js
+++ build/task-build-browser.js
@@ -1,13 +1,24 @@
 // Any copyright is dedicated to the Public Domain.
 // http://creativecommons.org/publicdomain/zero/1.0/
 
+import { SRC_DIR, SHARED_DIR } from './webpack.config.browser.default';
 import webpackProdConfig from './webpack.config.browser.prod';
 import webpackDevConfig from './webpack.config.browser.dev';
-import { webpackBuild, getBuildConfig } from './utils';
+import { shouldRebuild, getBuildConfig, webpackBuild } from './utils';
 import { logger } from './logging';
 
 export default async function() {
-  logger.info('Building browser...');
+  const id = 'browser';
+
+  const shouldRebuildSrc = await shouldRebuild(SRC_DIR, id);
+  const shouldRebuildShared = await shouldRebuild(SHARED_DIR, 'ui/shared');
+
+  if (!shouldRebuildSrc && !shouldRebuildShared) {
+    logger.info(`No changes in ${id}.`);
+    return { close: () => {} };
+  }
+
+  logger.info(`Building ${id}...`);
   const { development } = getBuildConfig();
   return await webpackBuild(development ? webpackDevConfig : webpackProdConfig);
 }
