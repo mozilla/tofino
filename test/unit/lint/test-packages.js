33039c108771121deb7300d855bb3cdf518f05f6
Victor Porof <vporof@mozilla.com>
2016-07-02 09:42:15 +0300

    Be stricter in test-packages.js and make sure the `ignored` list of packages doesn't include orphaned packages
    Signed-off-by: Victor Porof <vporof@mozilla.com>

--- test/unit/lint/test-packages.js
+++ test/unit/lint/test-packages.js
@@ -3,7 +3,6 @@
 
 import expect from 'expect';
 import fs from 'fs-promise';
-import difference from 'lodash/difference';
 import { autoFailingAsyncTest } from '../../utils/async';
 import { REQUIRES_REGEX, IMPORTS_REGEX, globMany, regexFiles } from './shared.js';
 
@@ -16,9 +15,19 @@ const paths = [
   `test${all}${valid}`,
 ];
 
+const native = [
+  'assert',
+  'child_process',
+  'electron',
+  'events',
+  'fs',
+  'os',
+  'path',
+  'stream',
+];
+
 const ignored = [
   // Temporary: these aren't yet referenced by the main app.
-  'datascript',
   'datascript-mori',
 
   // Build modules not imported anywhere, but used at the CLI.
@@ -55,7 +64,6 @@ const ignored = [
   'uglify-js',
   'eslint',
   'eslint-config-airbnb',
-  'eslint-plugin-flow-vars',
   'eslint-plugin-import',
   'eslint-plugin-jsx-a11y',
   'eslint-plugin-react',
@@ -66,6 +74,7 @@ describe('packages', () => {
   it('should not be orphaned', autoFailingAsyncTest(async function() {
     const manifest = await fs.readJson('package.json');
     const modules = [
+      ...native,
       ...Object.keys(manifest.dependencies),
       ...Object.keys(manifest.devDependencies),
     ];
@@ -74,6 +83,6 @@ describe('packages', () => {
     const matches = await regexFiles(sources, REQUIRES_REGEX, IMPORTS_REGEX);
     const expected = Array.from(new Set([...matches, ...ignored]));
 
-    expect(difference(modules, expected)).toEqual([]);
+    expect(expected.sort()).toEqual(modules.sort());
   }));
 });
