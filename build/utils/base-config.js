434f894666f1ac248470136fa4c79df3263911a9
Victor Porof <victor.porof@gmail.com>
2016-07-09 07:55:25 +0300

    Remove everything that's not used in the build-config (#768)
    Signed-off-by: Victor Porof <vporof@mozilla.com>

--- build/utils/base-config.js
+++ build/utils/base-config.js
@@ -1,22 +1,13 @@
 // Any copyright is dedicated to the Public Domain.
 // http://creativecommons.org/publicdomain/zero/1.0/
 
-import os from 'os';
 import { getElectronVersion } from './electron';
-import { getManifest } from './';
 
 export default {
-  // System information
-  platform: os.platform(),
-  arch: 'x64',
-
   // Electron information
   electron: getElectronVersion(),
 
-  // Version number, with extra build numbers if in a CI environment
-  version: getManifest().version,
-
   // The `development` option indicates whether or not the build is
-  // using hot reloading and unminified content and other things like that.
+  // using unoptimized, unminified content and other things like that.
   development: false,
 };
