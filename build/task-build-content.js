b3497358886013b730bd0a45e1ce9c42a823e867
Victor Porof <vporof@mozilla.com>
2016-07-18 10:55:20 +0300

    Make sure both the browser and content directories are rebuilt if something in the shared ui directory changes
    Signed-off-by: Victor Porof <vporof@mozilla.com>

--- build/task-build-content.js
+++ build/task-build-content.js
@@ -13,7 +13,7 @@ import { logger } from './logging';
 export default async function() {
   const id = 'content';
 
-  if (!(await shouldRebuild(id, [SRC_DIR, id], [SHARED_DIR, 'ui/shared']))) {
+  if (!(await shouldRebuild(id, [SRC_DIR, id], [SHARED_DIR, 'content ui/shared']))) {
     logger.info(colors.green(`No changes in ${id}.`));
     return { close: () => {} };
   }
