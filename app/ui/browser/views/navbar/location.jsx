4e82308a5421da21695dd0e1400bb567ad9d2a90
Jordan Santell <jsantell@mozilla.com>
2016-07-20 07:48:46 -0700

    Add a class to <body> that's platform dependent so we can have platform-specific styling. r=vporof,phlsa

--- app/ui/browser/views/navbar/location.jsx
+++ app/ui/browser/views/navbar/location.jsx
@@ -89,7 +89,6 @@ const INPUT_BAR_STYLE = Style.registerStyle({
     background: 'var(--theme-selection-color)',
     color: 'var(--theme-content-selected-color)',
   },
-  fontWeight: 100,
   fontSize: '120%',
 });
 
