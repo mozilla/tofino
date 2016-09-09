526b5252c241436a71f50dba757b15953b7af04c
Victor Porof <vporof@mozilla.com>
2016-09-09 12:47:29 +0200

    Consolidate tests regarding package use
    Signed-off-by: Victor Porof <vporof@mozilla.com>

--- test/unit/lint/test-dependencies.js
+++ test/unit/lint/test-dependencies.js
@@ -23,14 +23,8 @@ const native = [
   'url',
 ];
 
-// Only used during testing
-const ignored = [
-  'electron-ipc-mock',
-  'react-addons-perf',
-];
-
 describe('dependencies', () => {
-  it('processes only use app dependencies or main dependencies', async function() {
+  it('– processes only use app dependencies or main dependencies', async function() {
     const paths = [
       `app/main${all}${valid}`,
       `app/services${all}${valid}`,
@@ -38,48 +32,58 @@ describe('dependencies', () => {
       `app/ui${all}${valid}`,
     ];
 
+    const temporary = [
+      // These aren't yet referenced by the main app.
+      'datascript-mori',
+    ];
+
     const appManifest = await fs.readJson(path.join('app', 'package.json'));
     const mainManifest = await fs.readJson(path.join('package.json'));
-    const packages = Object.keys(appManifest.dependencies)
-                           .concat(Object.keys(mainManifest.dependencies))
-                           .sort();
+
+    const appPackages = Object.keys(appManifest.dependencies);
+    const mainPackages = Object.keys(mainManifest.dependencies);
+    const packages = without([...appPackages, ...mainPackages], ...temporary).sort();
 
     const sources = await globMany(paths);
     const matches = await regexFiles(sources, REQUIRES_REGEX, IMPORTS_REGEX);
-    const usedPackages = without(matches, ...native, ...ignored).sort();
+    const usedPackages = without(matches, ...native).sort();
 
     expect(usedPackages).toEqual(packages);
   });
 
-  it('test and build code only use main dependencies or devDependencies', async function() {
+  it('– build and test code only use main dependencies or devDependencies', async function() {
     const paths = [
       `build${all}${valid}`,
       `test${all}${valid}`,
     ];
 
-    const fullIgnored = [
-      ...ignored,
+    const temporary = [
+      // These aren't yet referenced by the main app.
+    ];
 
-      // Temporary: these aren't yet referenced by the main app.
-      'datascript-mori',
-
-      // Build modules not imported anywhere, but used at the CLI.
+    const ignored = [
+      // Modules not imported anywhere, but used at the CLI.
       'cross-env',
 
       // Test modules not imported anywhere, but used at the CLI.
-      'electron-mocha',
       'mocha',
+      'electron-mocha',
       'coveralls',
       'nyc',
+
+      // Required dynamically in mocha.opts
       'isomorphic-fetch',
 
       // Webpack loaders.
       'babel-loader',
       'json-loader',
-      // Needed by babel-loader
+
+      // Needed by babel-loader.
       'babel-core',
+      'babel-runtime',
 
       // Modules used only for configuration.
+      'babel-eslint',
       'babel-plugin-transform-async-to-generator',
       'babel-plugin-transform-class-properties',
       'babel-plugin-transform-es2015-destructuring',
@@ -89,27 +93,26 @@ describe('dependencies', () => {
       'babel-plugin-transform-runtime',
       'babel-preset-react-optimize',
       'babel-preset-react',
-      'babel-runtime',
-      'babel-eslint',
-      'uglify-js',
       'eslint-config-airbnb',
       'eslint-plugin-babel',
       'eslint-plugin-import',
       'eslint-plugin-jsx-a11y',
       'eslint-plugin-react',
       'react-addons-test-utils',
+      'uglify-js',
     ];
 
     const mainManifest = await fs.readJson('package.json');
-    const packages = without(Object.keys(mainManifest.devDependencies),
-                             ...fullIgnored).sort();
+    const mainPackages = Object.keys(mainManifest.dependencies);
+    const mainDevPackages = Object.keys(mainManifest.devDependencies);
+
+    const packages = without(mainDevPackages, ...temporary, ...ignored).sort();
 
     const sources = await globMany(paths);
     const matches = await regexFiles(sources, REQUIRES_REGEX, IMPORTS_REGEX);
-    // Strip out the main dependencies should leave only the devDependencies.
-    const usedPackages = without(matches, ...native,
-                                 ...Object.keys(mainManifest.dependencies)).sort();
 
+    // Strip out the main dependencies should leave only the devDependencies.
+    const usedPackages = without(matches, ...native, ...mainPackages).sort();
     expect(usedPackages).toEqual(packages);
   });
 });
