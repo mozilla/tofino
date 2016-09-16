mozilla/tofino

commit ae299301002a2b7fcff49bd960d0abd844940f9c

Reimplement net/cert error pages in browser-modern theme. Fixes #1072. r=vporof (#1107)

app/ui/browser-modern/views/browser/page/index.jsx
@@ -14,6 +14,8 @@ import React, { Component, PropTypes } from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import { connect } from 'react-redux';
 import { logger } from '../../../../../shared/logging';
+import ErrorPage from './error-page';
+import * as SharedPropTypes from '../../../model/shared-prop-types';
 
 import Style from '../../../../shared/style';
 import PageState from '../../../model/page-state';
@@ -97,26 +99,65 @@ class Page extends Component {
     this.webview.addEventListener('did-finish-load', () => {
       // Event fired when the navigation is done and onload event is dispatched.
       // May be emitted multiple times for every single frame.
-      // Not emitted when a failure occurs, unlike `did-stop-loading`.
       // Might not always be emitted after a `did-start-loading`.
-    });
-
-    this.webview.addEventListener('did-fail-load', () => {
-      // Like 'did-finish-load', but fired when the load failed or was cancelled.
-      // May be emitted multiple times for every single frame.
-      // Only emitted when a failure occurs, unlike `did-stop-loading`.
-      this.props.dispatch(PageActions.setPageState(this.props.pageId, {
-        load: PageState.STATES.FAILED,
-      }));
     });
 
     this.webview.addEventListener('did-stop-loading', () => {
       // Event fired when a page finishes loading.
       // May be emitted multiple times for every single frame.
       // Emitted regardless of whether or not a failure occurs.
-      this.props.dispatch(PageActions.setPageState(this.props.pageId, {
-        load: PageState.STATES.LOADED,
+      const title = this.webview.getTitle();
+      const url = this.webview.getURL();
+
+      // `page-title-set` is not called on error pages, nor is
+      // `did-navigate` called, so we must set these properties to
+      // properly render our error pages
+      this.props.dispatch(PageActions.setPageDetails(this.props.pageId, {
+        title,
+        location: url,
       }));
+
+      // If the page state is still LOADING, and we haven't hit a
+      // failure state, mark this page as LOADED.
+      if (this.props.pageState.load === PageState.STATES.LOADING) {
+        this.props.dispatch(PageActions.setPageState(this.props.pageId, {
+          load: PageState.STATES.LOADED,
+        }));
+      }
+    });
+
+    this.webview.addEventListener('did-fail-load', (e) => {
+      // Like 'did-finish-load', but fired when the load failed or was cancelled.
+      // May be emitted multiple times for every single frame.
+      // Only emitted when a failure occurs, unlike `did-stop-loading`.
+      // Is fired before did-finish-load and did-stop-loading when failure occurs.
+      const { errorCode, errorDescription, validatedURL, isMainFrame } = e;
+
+      if (!isMainFrame) {
+        return;
+      }
+
+      // If a page is aborted (like hitting BACK/RELOAD while a page is loading), we'll get a
+      // load failure of ERR_ABORTED with code `-3` -- this is intended, so just ignore it.
+      if (Math.abs(errorCode) === 3) {
+        return;
+      }
+
+      const state = {
+        load: PageState.STATES.FAILED,
+        code: errorCode,
+        description: errorDescription,
+      };
+
+      // Most cert errors are 501 from this event; the true, more descriptive
+      // error description can be retrieved from the main process. Just check
+      // here roughly if the error looks like a cert error.
+      if (Math.abs(errorCode) === 501 || /CERT/.test(errorDescription)) {
+        this.props.dispatch(PageEffects.getCertificateError(this.props.pageId, validatedURL));
+      }
+
+      this.props.dispatch(PageActions.setPageState(this.props.pageId, state));
+      this.props.dispatch(UIEffects.setURLBarValue(this.props.pageId, validatedURL));
     });
 
     this.webview.addEventListener('page-title-set', e => {
@@ -164,6 +205,10 @@ class Page extends Component {
     return (
       <div id={`browser-page-${this.props.pageId}`}
         className={`browser-page ${PAGE_STYLE}`}>
+        <ErrorPage
+          hidden={this.props.pageState.load !== PageState.STATES.FAILED}
+          url={this.props.pageLocation}
+          pageState={this.props.pageState} />
         <webview is="webview"
           ref={e => this.webview = e}
           class={WEB_VIEW_STYLE}
@@ -177,8 +222,18 @@ Page.displayName = 'Page';
 
 Page.propTypes = {
   dispatch: PropTypes.func.isRequired,
-  pageId: PropTypes.string.isRequired,
   onMount: PropTypes.func.isRequired,
+  pageId: PropTypes.string.isRequired,
+  pageLocation: PropTypes.string.isRequired,
+  pageState: SharedPropTypes.PageState.isRequired,
 };
 
-export default connect()(Page);
+function mapStateToProps(state, ownProps) {
+  const page = PagesSelectors.getPageById(state, ownProps.pageId);
+  return {
+    pageLocation: page ? page.location : '',
+    pageState: PagesSelectors.getPageState(state, ownProps.pageId),
+  };
+}
+
+export default connect(mapStateToProps)(Page);

.eslintrc
@@ -72,7 +72,7 @@
     "babel/generator-star-spacing": ["error", { before: false, after: true }],
 
     "react/display-name": ["error", { ignoreTranspilerName: true }],
-
+    "react/prefer-stateless-function": ["off"],
     "react/jsx-closing-bracket-location": ["error", "after-props"],
     "react/jsx-max-props-per-line": ["error", { "maximum": 1 }],
     "react/jsx-first-prop-new-line": ["off"],

app/ui/browser-modern/actions/page-effects.js
@@ -194,3 +194,11 @@ export function captureCurrentPage() {
     dispatch(capturePage(PagesSelectors.getSelectedPageId(getState())));
   };
 }
+
+export function getCertificateError(pageId, url) {
+  return {
+    type: EffectTypes.GET_CERTIFICATE_ERROR,
+    pageId,
+    url,
+  };
+}

app/ui/browser-modern/constants/effect-types.js
@@ -41,6 +41,7 @@ export const TOGGLE_DEVTOOLS = 'TOGGLE_DEVTOOLS_EFFECT';
 export const PERFORM_PAGE_SEARCH = 'PERFORM_PAGE_SEARCH_EFFECT';
 export const SET_PAGE_ZOOM_LEVEL = 'SET_PAGE_ZOOM_LEVEL_EFFECT';
 export const CAPTURE_PAGE = 'CAPTURE_PAGE_EFFECT';
+export const GET_CERTIFICATE_ERROR = 'GET_CERTIFICATE_ERROR_EFFECT';
 
 // UI effects
 

app/ui/browser-modern/css/theme.css
@@ -41,4 +41,10 @@
 
   --theme-default-shadow: 0px 2px 4px 0px rgba(0,0,0,0.5);
   --theme-default-selected-shadow: 0px 0px 6px 0px #f00;
+
+  /* Error Pages */
+
+  --theme-error-page-background-color: #FBFBFB;
+  --theme-error-page-color: rgb(66, 78, 90);
+  --theme-error-page-cert-background-color: '#ddd';
 }

app/ui/browser-modern/model/page-state.js
@@ -12,12 +12,18 @@ specific language governing permissions and limitations under the License.
 
 import Immutable from 'immutable';
 
+import SSLCertificate from './ssl-certificate';
+
 const PageState = Immutable.Record({
   load: undefined,
   canGoBack: false,
   canGoForward: false,
   canRefresh: false,
   zoomLevel: 0,
+  code: undefined,
+  description: undefined,
+  certificate: new SSLCertificate(),
+  error: undefined,
 }, 'PageState');
 
 PageState.STATES = {

app/ui/browser-modern/model/shared-prop-types.js
@@ -18,6 +18,8 @@ import ProfileModel from './profile';
 import PageModel from './page';
 import PageMetaModel from './page-meta';
 import LocationAutocompletionModel from './location-autocompletion';
+import PageStateModel from './page-state';
+import SSLCertificateModel from './ssl-certificate';
 
 export const WebViewController = PropTypes.instanceOf(WebViewControllerModel);
 
@@ -30,7 +32,9 @@ export const Page = PropTypes.instanceOf(PageModel);
 export const Pages = ImmutablePropTypes.listOf(Page.isRequired);
 
 export const PageMeta = PropTypes.instanceOf(PageMetaModel);
+export const PageState = PropTypes.instanceOf(PageStateModel);
 export const PageMetas = ImmutablePropTypes.listOf(PageMeta.isRequired);
+export const SSLCertificate = PropTypes.instanceOf(SSLCertificateModel);
 
 export const LocationAutocompletion = PropTypes.instanceOf(LocationAutocompletionModel);
 export const LocationAutocompletions = ImmutablePropTypes.listOf(LocationAutocompletion);

app/ui/browser-modern/model/ssl-certificate.js
@@ -1,0 +1,25 @@
+/*
+Copyright 2016 Mozilla
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+*/
+
+import Immutable from 'immutable';
+
+const SSLCertificate = Immutable.Record({
+  data: undefined,
+  issuerName: undefined,
+  subjectName: undefined,
+  serialNumber: undefined,
+  validStart: undefined,
+  validExpiry: undefined,
+  fingerprint: undefined,
+}, 'SSLCertificate');
+
+export default SSLCertificate;

commit a8fb9e13fd56a247c80616b1b7bb869128839953

Fix the location bar not updating with the new page url in some cases, regressed by ae29930
Signed-off-by: Victor Porof <vporof@mozilla.com>

app/ui/browser-modern/views/browser/page/index.jsx
@@ -83,7 +83,7 @@ class Page extends Component {
       // Like `did-navigate`, but fired only when an in-page navigation happens,
       // meaning that the page URL changes but does not cause navigation outside
       // of the page (e.g. when the hashchange event is triggered).
-      if (!e.isMainFrame || e.url === this.props.pageLocation) {
+      if (!e.isMainFrame) {
         return;
       }
       this.props.dispatch(PageActions.setPageDetails(this.props.pageId, { location: e.url }));
@@ -102,15 +102,13 @@ class Page extends Component {
       // May be emitted multiple times for every single frame.
       // Emitted regardless of whether or not a failure occurs.
       const title = this.webview.getTitle();
-      const url = this.webview.getURL();
+      const location = this.webview.getURL();
 
-      // `page-title-set` is not called on error pages, nor is
-      // `did-navigate` called, so we must set these properties to
+      // The `page-title-set` event is not called on error pages, nor is
+      // the `did-navigate` event called, so we must set these properties to
       // properly render our error pages
-      this.props.dispatch(PageActions.setPageDetails(this.props.pageId, {
-        title,
-        location: url,
-      }));
+      this.props.dispatch(PageActions.setPageDetails(this.props.pageId, { title, location }));
+      this.props.dispatch(UIEffects.setURLBarValue(this.props.pageId, location));
 
       // If the page state is still LOADING, and we haven't hit a
       // failure state, mark this page as LOADED.