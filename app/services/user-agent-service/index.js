4c8443e9be84a9c24be57121d88daea27ef73f7b
Nick Alexander <nalexander@mozilla.com>
2016-09-15 17:56:03 -0700

    Pre: Don't process.exit the importing process by default.
    I was witnessing incredibly difficult to trace "exit code 2" from Mocha
    when *importing* user-agent-service/index.js.  What was happening is
    that the import bound process events to process.exit, and
    other (unrelated!) tests had unhandled rejections.  This avoids the
    unsightly exit, and also updates the exit codes to give a hint of the
    issue in the future.  (Exit codes 1 and 2 are valid Mocha exit codes.)

--- app/services/user-agent-service/index.js
+++ app/services/user-agent-service/index.js
@@ -18,17 +18,6 @@ import * as userAgentService from './server';
 import { ProfileStorage } from './sqlstorage';
 import meta from './meta.json';
 
-process.on('uncaughtException', err => {
-  logger.error(err.stack);
-  process.exit(1);
-});
-
-process.on('unhandledRejection', (reason, p) => {
-  logger.error(`Unhandled Rejection at: Promise ${JSON.stringify(p)}`);
-  logger.error(reason.stack);
-  process.exit(2);
-});
-
 export async function UserAgentService(options = {}) {
   if (typeof options.port !== 'number') {
     throw new Error('UserAgentService requires a `port` number.');
