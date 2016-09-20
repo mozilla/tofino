f0bdc8bf6c6d6844be0e13bdf7918d13b0a51da5
Jordan Santell <jsantell@users.noreply.github.com>
2016-09-20 10:20:45 -0700

    Set sourcemaps back to eval mode, due to page-metadata-parser not working currently anyway, and 'eval' gives us true authored source. r=bgrins (#1178)

--- build/config/webpack.base.snippets.dev.js
+++ build/config/webpack.base.snippets.dev.js
@@ -7,10 +7,7 @@ export default {
   output: {
     pathinfo: true,
   },
-  // Something in `fathom-web/utils.js` breaks when eval'd,
-  // so don't use the 'eval' tool for now, even though it's a
-  // cheaper sourcemap generator.
-  devtool: 'cheap-source-map',
+  devtool: 'eval',
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': '"development"',
