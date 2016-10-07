c86b3b84c915a84fd25fc7242d20ca7ad3db3eeb
Dave Townsend <dtownsend@oxymoronical.com>
2016-10-07 16:25:17 -0700

    Add Tofino to the registry as a handler for html and http and parse urls passed on the command line.

--- app/main/protocols.js
+++ app/main/protocols.js
@@ -11,8 +11,14 @@ specific language governing permissions and limitations under the License.
 */
 
 import { protocol, app } from 'electron';
+import tmp from 'tmp';
+import thenify from 'thenify';
+import fs from 'fs-promise';
+import childProcess from 'child_process';
 import * as endpoints from '../shared/constants/endpoints';
 
+const mktmp = thenify(tmp.file);
+const exec = thenify(childProcess.exec);
 const tofinoColonSlash = new RegExp(`^${endpoints.TOFINO_PROTOCOL}:/`);
 
 export const DEFAULT_PROTOCOLS = ['http', 'https', endpoints.TOFINO_PROTOCOL];
@@ -52,4 +58,94 @@ export function setDefaultBrowser() {
 
 export function isDefaultBrowser() {
   return DEFAULT_PROTOCOLS.every(p => app.isDefaultProtocolClient(p));
+}
+
+async function mergeRegistry(text) {
+  const [path, fd] = await mktmp();
+
+  await fs.write(fd, text, 0, 'utf8');
+  await fs.close(fd);
+  await exec(`regedit.exe /s ${path}`);
+}
+
+export async function addToWindowsRegistry() {
+  const exepath = process.execPath.replace(/\\/g, '\\\\');
+  await mergeRegistry(`Windows Registry Editor Version 5.00
+
+[HKEY_CLASSES_ROOT\\TofinoHTML]
+@="Tofino HTML Document"
+
+[HKEY_CLASSES_ROOT\\TofinoHTML\\DefaultIcon]
+@="${exepath},0"
+
+[HKEY_CLASSES_ROOT\\TofinoHTML\\shell]
+
+[HKEY_CLASSES_ROOT\\TofinoHTML\\shell\\open]
+
+[HKEY_CLASSES_ROOT\\TofinoHTML\\shell\\open\\command]
+@="\\"${exepath}\\" -- \\"%1\\""
+
+[HKEY_CURRENT_USER\\SOFTWARE\\Classes\\TofinoHTML]
+@="Tofino HTML Document"
+
+[HKEY_CURRENT_USER\\SOFTWARE\\Classes\\TofinoHTML\\DefaultIcon]
+@="${exepath},0"
+
+[HKEY_CURRENT_USER\\SOFTWARE\\Classes\\TofinoHTML\\shell]
+
+[HKEY_CURRENT_USER\\SOFTWARE\\Classes\\TofinoHTML\\shell\\open]
+
+[HKEY_CURRENT_USER\\SOFTWARE\\Classes\\TofinoHTML\\shell\\open\\command]
+@="\\"${exepath}\\" -- \\"%1\\""
+
+[HKEY_CURRENT_USER\\SOFTWARE\\Clients\\StartMenuInternet\\Tofino]
+@="Tofino"
+
+[HKEY_CURRENT_USER\\SOFTWARE\\Clients\\StartMenuInternet\\Tofino\\Capabilities]
+"ApplicationDescription"="Project Tofino"
+"ApplicationName"="Tofino"
+
+[HKEY_CURRENT_USER\\SOFTWARE\\Clients\\StartMenuInternet\\Tofino\\Capabilities\\FileAssociations]
+".htm"="TofinoHTML"
+".html"="TofinoHTML"
+".shtml"="TofinoHTML"
+".xht"="TofinoHTML"
+".xhtml"="TofinoHTML"
+".webp"="TofinoHTML"
+
+[HKEY_CURRENT_USER\\SOFTWARE\\Clients\\StartMenuInternet\\Tofino\\Capabilities\\StartMenu]
+"StartMenuInternet"="Tofino"
+
+[HKEY_CURRENT_USER\\SOFTWARE\\Clients\\StartMenuInternet\\Tofino\\Capabilities\\URLAssociations]
+"ftp"="TofinoHTML"
+"http"="TofinoHTML"
+"https"="TofinoHTML"
+
+[HKEY_CURRENT_USER\\SOFTWARE\\Clients\\StartMenuInternet\\Tofino\\InstallInfo]
+"IconsVisible"=dword:00000001
+
+[HKEY_CURRENT_USER\\SOFTWARE\\Clients\\StartMenuInternet\\Tofino\\shell]
+
+[HKEY_CURRENT_USER\\SOFTWARE\\Clients\\StartMenuInternet\\Tofino\\shell\\open]
+
+[HKEY_CURRENT_USER\\SOFTWARE\\Clients\\StartMenuInternet\\Tofino\\shell\\open\\command]
+@="${exepath}"
+
+[HKEY_CURRENT_USER\\SOFTWARE\\RegisteredApplications]
+"Tofino"="Software\\\\Clients\\\\StartMenuInternet\\\\Tofino\\\\Capabilities"
+`);
+}
+
+export async function removeFromWindowsRegistry() {
+  await mergeRegistry(`Windows Registry Editor Version 5.00
+
+[-HKEY_CLASSES_ROOT\\TofinoHTML]
+
+[-HKEY_CURRENT_USER\\SOFTWARE\\Classes\\TofinoHTML]
+
+[-HKEY_CURRENT_USER\\SOFTWARE\\Clients\\StartMenuInternet\\Tofino]
+
+[HKEY_CURRENT_USER\\SOFTWARE\\RegisteredApplications]
+"Tofino"=-
+`);
 }
