86a15cd60423feaae45de1b7ca3db45b769630c9
Victor Porof <vporof@mozilla.com>
2016-10-10 10:10:50 +0200

    Stop importing the 'electron' module in ui code
    Signed-off-by: Victor Porof <vporof@mozilla.com>

--- app/ui/preload/active-element.js
+++ app/ui/preload/active-element.js
@@ -10,7 +10,7 @@ CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 */
 
-import { ipcRenderer as ipc } from 'electron';
+import { ipcRenderer as ipc } from '../../shared/electron';
 import { serializeNode } from '../shared/util/dom-serializers';
 
 document.addEventListener('blur', () => {
