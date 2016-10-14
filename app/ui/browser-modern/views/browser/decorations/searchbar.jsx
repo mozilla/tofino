cc64e05b1d22a61ba439f8e274929a1ea552ec5a
Jordan Santell <jsantell@users.noreply.github.com>
2016-10-14 13:05:05 -0400

    Add a close button to the find bar. Fixes #1397. r=vporof (#1408)

--- app/ui/browser-modern/views/browser/decorations/searchbar.jsx
+++ app/ui/browser-modern/views/browser/decorations/searchbar.jsx
@@ -16,13 +16,14 @@ import { connect } from 'react-redux';
 
 import Style from '../../../../shared/style';
 import Search from '../../../../shared/widgets/search';
+import Btn from '../../../../shared/widgets/btn';
 
 import * as UIConstants from '../../../constants/ui';
 import * as PagesSelectors from '../../../selectors/pages';
 import * as PageActions from '../../../actions/page-actions';
 import * as PageEffects from '../../../actions/page-effects';
 
-const SEARCH_STYLE = Style.registerStyle({
+const SEARCH_BAR_STYLE = Style.registerStyle({
   position: 'fixed',
   bottom: 0,
   left: 0,
@@ -30,8 +31,19 @@ const SEARCH_STYLE = Style.registerStyle({
   height: '25px',
   padding: '0 4px',
   zIndex: UIConstants.SEARCH_BAR_INDEX,
-  borderTop: '1px solid var(--theme-content-border-color)',
+  border: '1px solid var(--theme-content-border-color)',
   color: 'var(--theme-content-color)',
+  backgroundColor: 'var(--theme-content-background)',
+  width: '300px',
+  alignItems: 'center',
+});
+
+const CLOSE_BUTTON_STYLE = Style.registerStyle({
+  marginRight: '3px',
+});
+
+const SEARCH_INPUT_STYLE = Style.registerStyle({
+  width: '300px',
 });
 
 class SearchBar extends Component {
@@ -70,14 +82,31 @@ class SearchBar extends Component {
     }
   }
 
+  handleClose = () => {
+    this.props.dispatch(PageActions.hidePageSearch(this.props.pageId));
+  }
+
   render() {
     return (
-      <Search ref={e => this.searchbar = e}
-        className={`browser-page-searchbar ${SEARCH_STYLE}`}
-        hidden={!this.props.isVisible}
-        onChange={this.handleInputChange}
-        onKeyDown={this.handleInputKeyDown}
-        placeholder={'Search...'} />
+      <div className={`browser-page-searchbar-container ${SEARCH_BAR_STYLE}`}
+        hidden={!this.props.isVisible}>
+        <Btn className={`search-close-button ${CLOSE_BUTTON_STYLE}`}
+          title="Close search"
+          width="14px"
+          height="14px"
+          image="close.png"
+          imgWidth="64px"
+          imgHeight="16px"
+          imgPosition="-1px -1px"
+          imgPositionHover="-17px -1px"
+          imgPositionActive="-33px -1px"
+          onClick={this.handleClose} />
+        <Search ref={e => this.searchbar = e}
+          className={`browser-page-searchbar ${SEARCH_INPUT_STYLE}`}
+          onChange={this.handleInputChange}
+          onKeyDown={this.handleInputKeyDown}
+          placeholder={'Search...'} />
+      </div>
     );
   }
 }
