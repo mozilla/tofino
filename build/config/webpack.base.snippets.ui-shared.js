e05cfb2f70ba7d4ab9fa237f5eee94036e18dcc2
Victor Porof <vporof@mozilla.com>
2016-10-23 12:13:24 +0200

    Remove duplicated code between all frontend webpack configs
    Signed-off-by: Victor Porof <vporof@mozilla.com>

---
+++ build/config/webpack.base.snippets.ui-shared.js
@@ -0,0 +1,40 @@
+// Any copyright is dedicated to the Public Domain.
+// http://creativecommons.org/publicdomain/zero/1.0/
+
+import path from 'path';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+
+export const uiFiles = (srcDir, dstDir) => ([
+  new CopyWebpackPlugin([{
+    from: path.join(srcDir, '*.html'),
+    to: dstDir,
+    flatten: true,
+  }]),
+  new CopyWebpackPlugin([{
+    from: path.join(srcDir, 'css', '*.css'),
+    to: path.join(dstDir, 'css'),
+    flatten: true,
+  }]),
+  new CopyWebpackPlugin([{
+    from: path.join(srcDir, 'favicon.ico'),
+    to: path.join(dstDir, 'favicon.ico'),
+  }]),
+]);
+
+export const sharedFiles = (sharedDir, dstDir) => ([
+  new CopyWebpackPlugin([{
+    from: path.join(sharedDir, 'css', '*.css'),
+    to: path.join(dstDir, 'css'),
+    flatten: true,
+  }]),
+  new CopyWebpackPlugin([{
+    from: path.join(sharedDir, 'assets'),
+    to: path.join(dstDir, 'assets'),
+    flatten: true,
+  }]),
+  new CopyWebpackPlugin([{
+    from: path.join(sharedDir, 'fonts'),
+    to: path.join(dstDir, 'fonts'),
+    flatten: true,
+  }]),
+]);
